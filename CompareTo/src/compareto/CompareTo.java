d35b00d9b5013536e25de9f5f368e361fde3b0c4
Katarina Inezita <109232320+katarinainezita@users.noreply.github.com>
2022-11-15 18:09:44 +0700

    Add files via upload

---
+++ CompareTo/src/compareto/CompareTo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compareto;
+
+/**
+ *
+ * @author Inezita
+ */
+public class CompareTo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s1="hello";  
+        String s2="hello";  
+        String s3="meklo";  
+        String s4="hemlo";  
+        String s5="flag";  
+        System.out.println(s1.compareTo(s2));   // TODO code application logic here
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareTo(s4));
+        System.out.println(s1.compareTo(s5));
+    }
+    
+}
